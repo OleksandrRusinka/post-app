cafbcc6bfb826d14d87c48915afe5a8619a54743
Sasha Rusinka <o.rusinka@rubylabs.com>
2025-10-03 17:51:06 +0300

    env

--- src/config/env/env.client.ts
+++ src/config/env/env.client.ts
@@ -7,7 +7,6 @@ export const envClient = createEnv({
   client: {
     NEXT_PUBLIC_API_BASE_URL: z.string().url().min(1, { message: 'NEXT_PUBLIC_API_BASE_URL is required' }),
     NEXT_PUBLIC_APP_NAME: z.string().min(1, { message: 'NEXT_PUBLIC_APP_NAME is required' }),
-    NEXT_PUBLIC_APP_URL: z.string().url().min(1, { message: 'NEXT_PUBLIC_APP_URL is required' }),
     NEXT_PUBLIC_SENTRY_DSN: z.string().url().optional().or(z.literal('')),
     NEXT_PUBLIC_SENTRY_ENV: z.string().optional().or(z.literal('')),
     NEXT_PUBLIC_SUPABASE_URL: z.string().url().min(1, { message: 'NEXT_PUBLIC_SUPABASE_URL is required' }),
@@ -21,7 +20,7 @@ export const envClient = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+
     NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
     NEXT_PUBLIC_SENTRY_ENV: process.env.NEXT_PUBLIC_SENTRY_ENV,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
