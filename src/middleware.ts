b6f9a8a2af7168731485072c32aff7ccf13b9242
Sasha Rusinka <o.rusinka@rubylabs.com>
2025-10-08 15:12:34 +0300

    improve

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,5 @@ export default createMiddleware(routing)
 
 // config
 export const config = {
-  matcher: [
-    '/((?!api|trpc|_next|_next/static|_next/image|_vercel|static|.well-known|admin|fonts|sitemap|images|icons|robots|webmanifest|.*\\.xml$|.*\\.webp$|.*\\.avif$|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.ico$|.*\\.svg$|.*\\.txt$|.*\\.js$|.*\\.css$).*)',
-  ],
+  matcher: ['/((?!api|_next|_next/static|_next/image|.*\\.ico$|.*\\.svg$|.*\\.png$|.*\\.txt$|.*\\.js$|.*\\.css$).*)'],
 }
